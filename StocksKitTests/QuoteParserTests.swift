5c7fe0269dac51e59f87f64c0c503d0e12e751a6
Alexander Edge <alex@alexedge.co.uk>
2016-01-27 16:33:49 +0000

    Add tests for quote parsing

---
+++ StocksKitTests/QuoteParserTests.swift
@@ -0,0 +1,47 @@
+//
+//  QuoteTest.swift
+//  StocksKit
+//
+//  Created by Alexander Edge on 20/01/2016.
+//  Copyright © 2016 Alexander Edge. All rights reserved.
+//
+
+import XCTest
+@testable import StocksKit
+
+class QuoteParserTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    private func testJSON() throws -> [String: AnyObject] {
+        return try NSJSONSerialization.JSONObjectWithData(NSData(contentsOfFile: NSBundle(forClass: self.dynamicType).pathForResource("Quotes", ofType: "json")!)!, options: []) as! [String: AnyObject]
+    }
+    
+    func testParsesValidJSON() {
+        
+        do {
+
+            let quote = try QuoteParser().parse(testJSON())
+            
+            XCTAssertEqual(quote.symbol, "ARM.L")
+            XCTAssertEqual(quote.lastTradePrice, NSDecimalNumber(double: 1001.0000))
+            XCTAssertEqual(quote.currency, "GBp")
+            XCTAssertEqual(quote.exchange, "LSE")
+            XCTAssertEqual(quote.change, NSDecimalNumber(double: -13.9999))
+            XCTAssertEqual(quote.percentChange, NSDecimalNumber(string: -0.013793))
+            
+        } catch {
+            XCTFail()
+        }
+        
+    }
+    
+}

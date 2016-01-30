9267f104bd0b4f409c53ab83002d158a9e84004d
Alexander Edge <alex@alexedge.co.uk>
2016-01-30 19:41:27 +0000

    Add date formatter

--- StocksKit/ExchangeRateParser.swift
+++ StocksKit/ExchangeRateParser.swift
@@ -8,13 +8,40 @@
 
 import Foundation
 
-struct ExchangeRateParser : JSONParsingType {
+private struct ExchangeRateDateFormatter {
+    
+    private static let formatter: NSDateFormatter = {
+        let formatter = NSDateFormatter()
+        formatter.dateFormat = "MM/dd/yyyy HH:mma"
+        return formatter
+        
+    }()
+    
+    static func dateFromString(string : String) -> NSDate? {
+        return formatter.dateFromString(string)
+    }
+    
+}
+
+
+internal struct ExchangeRateParser : JSONParsingType {
     typealias T = ExchangeRate
+    
+    private static var dateFormatter: NSDateFormatter = {
+       
+        let formatter = NSDateFormatter()
+        formatter.dateFormat = "MM/dd/yyyy HH:mma"
+        return formatter
+        
+    }()
     
     enum ExchangeRateError : ErrorType {
         case MissingOrInvalidIdentifier
         case MissingOrInvalidName
         case MissingOrInvalidRate
+        case MissingOrInvalidDate
+        case MissingOrInvalidTime
+        case UnableToParseDate
     }
     
     func parse(json: [String : AnyObject]) throws -> T {
@@ -27,11 +54,32 @@ struct ExchangeRateParser : JSONParsingType {
             throw ExchangeRateError.MissingOrInvalidName
         }
         
+        let currencies = name.componentsSeparatedByString("/")
+        guard let from = currencies.first else {
+            throw ExchangeRateError.MissingOrInvalidName
+        }
+        
+        guard let to = currencies.last else {
+            throw ExchangeRateError.MissingOrInvalidName
+        }
+        
         guard let rate = json["Rate"] as? String else {
             throw ExchangeRateError.MissingOrInvalidRate
         }
         
-        return ExchangeRate(identifier: identifier, name: name, rate: NSDecimalNumber(string: rate))
+        guard let dateString = json["Date"] as? String else {
+            throw ExchangeRateError.MissingOrInvalidDate
+        }
+        
+        guard let timeString = json["Time"] as? String else {
+            throw ExchangeRateError.MissingOrInvalidTime
+        }
+        
+        guard let date = ExchangeRateDateFormatter.dateFromString([dateString,timeString].joinWithSeparator(" ")) else {
+            throw ExchangeRateError.UnableToParseDate
+        }
+
+        return ExchangeRate(identifier: identifier, from: from, to: to, rate: NSDecimalNumber(string: rate), date: date)
         
     }
     

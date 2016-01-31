f23f03a6c8c77b2c0c7c5d4274b1e1b00c41bedf
Alexander Edge <alex@alexedge.co.uk>
2016-01-31 15:14:40 +0000

    Remove methods not required

--- StocksKit/Portfolio.swift
+++ StocksKit/Portfolio.swift
@@ -10,17 +10,10 @@ import Foundation
 
 final public class Portfolio: NSObject, NSCoding {
     
-    enum PortfolioError : ErrorType {
-        case MissingQuote
-    }
-    
     public var holdings: [Holding]
     public var currency: String
     
-    private let exchangeRates: [String: NSDecimalNumber] = ["USD" : 0.696758, "GBp": 0.01, "GBP": 1]
-    private let quotes: [String: Quote] = [:]
-    
-    public init(holdings: [Holding], currency: String) {
+    public init(holdings: [Holding] = [], currency: String) {
         self.holdings = holdings
         self.currency = currency
     }
@@ -40,59 +33,12 @@ final public class Portfolio: NSObject, NSCoding {
         aCoder.encodeObject(self.currency, forKey: SerializationKeys.Currency.rawValue)
     }
     
-    private func verifyQuotesForHoldings(quotes: [Quote], holdings: [Holding]) throws {
-        guard Set(holdings.map({$0.symbol})).isSubsetOf(Set(quotes.map({$0.symbol}))) else {
-            throw PortfolioError.MissingQuote
-        }
-    }   
-    
 }
 
 extension Portfolio {
     
     public var cost: NSDecimalNumber {
         return holdings.reduce(0){$0 + $1.cost}
-    }
-    
-    public var symbols: [String] {
-        return Array(Set(holdings.map({$0.symbol})))
-    }
-    
-    public func gain(quotes: [Quote]) throws -> NSDecimalNumber {
-        try verifyQuotesForHoldings(quotes, holdings: holdings)
-        return quotes.reduce(0) {
-            let quote = $1
-            let holdings: [Holding] = self.holdings.filter({$0.symbol == quote.symbol})
-            if let exchangeRate = exchangeRates[quote.currency] {
-                return $0 + holdings.reduce(0){
-                    do {
-                        return try $1.value(quote, exchangeRate: exchangeRate) + $0
-                    } catch {
-                        return $0
-                    }} - holdings.reduce(0){$0 + $1.cost}
-            } else {
-                return $0
-            }
-        }
-    }
-    
-    public func value(quotes: [Quote]) throws -> NSDecimalNumber {
-        try verifyQuotesForHoldings(quotes,holdings: holdings)
-        return quotes.reduce(0) {
-            let quote = $1
-            let holdings: [Holding] = self.holdings.filter({$0.symbol == quote.symbol})
-            if let exchangeRate = exchangeRates[quote.currency] {
-                return $0 + holdings.reduce(0){
-                    do {
-                        return try $1.value(quote, exchangeRate: exchangeRate) + $0
-                    } catch {
-                        return $0
-                    }
-                }
-            } else {
-                return $0
-            }
-        }
     }
     
 }

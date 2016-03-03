47e2df79d913da3a19287bb19768aae700193c85
Alexander Edge <alex@alexedge.co.uk>
2016-03-03 14:20:21 +0000

    Update licenses

--- Sources/StocksKit/Model/Quote.swift
+++ Sources/StocksKit/Model/Quote.swift
@@ -1,14 +1,32 @@
 //
 //  Quote.swift
-//  Stocks
+//  StocksKit
 //
-//  Created by Alexander Edge on 19/01/2016.
-//  Copyright © 2016 Alexander Edge. All rights reserved.
+//  The MIT License (MIT)
 //
+//  Copyright (c) 2016 Alexander Edge
+//
+//  Permission is hereby granted, free of charge, to any person obtaining a copy
+//  of this software and associated documentation files (the "Software"), to deal
+//  in the Software without restriction, including without limitation the rights
+//  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+//  copies of the Software, and to permit persons to whom the Software is
+//  furnished to do so, subject to the following conditions:
+//
+//  The above copyright notice and this permission notice shall be included in all
+//  copies or substantial portions of the Software.
+//
+//  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+//  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+//  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+//  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+//  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+//  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+//  SOFTWARE.
 
 import Foundation
 
-public struct Quote : QuoteType {
+public struct Quote {
     
     public let symbol : String
     public let name : String
@@ -49,6 +67,7 @@ extension NSURLSession {
     }
     
 }
+
 extension CollectionType where Generator.Element: QuoteType {
     
     public var currencies: [String] {
